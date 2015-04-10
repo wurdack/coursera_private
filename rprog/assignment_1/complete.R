34adfe3bbc7c7510d1e770f99ac7b11abc58c1bb
Richard Wurdack <rwurdack@jawbone.com>
2015-04-09 17:29:02 -0700

    Part 2

--- rprog/assignment_1/complete.R
+++ rprog/assignment_1/complete.R
@@ -1,3 +1,12 @@
+count_complete <- function(frame) {
+        ## Pull an array of sulfate readings from the frame
+        sulf_present <- !is.na(frame[,"sulfate"])
+        nitr_present <- !is.na(frame[,"nitrate"])
+
+        ## Count the number of instances where both are present.
+        sum(sulf_present & nitr_present)
+}
+
 complete <- function(directory, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
@@ -12,4 +21,17 @@ complete <- function(directory, id = 1:332) {
         ## ...
         ## where 'id' is the monitor ID number and 'nobs' is the
         ## number of complete cases
+
+        ## filenames is a vector of monitor CSV files.
+        ids <- sprintf("%03d.csv",id)
+        filenames <- file.path(directory, ids)
+
+        ## Frames is a list of data frames corresponding to each file
+        frames <- lapply(filenames, read.csv)
+
+        ## From each frame, pull an array of readings
+        nobs <- sapply(frames, count_complete)
+
+        ## Package the results into a data frame
+        data.frame(id, nobs)
 }
