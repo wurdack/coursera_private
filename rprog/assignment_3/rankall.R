29e6ea113293673e0f2c3476fd9c8d7be7abe86b
Richard Wurdack <wurdack@hotmail.com>
2015-04-19 14:50:32 -0700

    [part2][part3] Success.

--- rprog/assignment_3/rankall.R
+++ rprog/assignment_3/rankall.R
@@ -1,9 +1,54 @@
+rankhospital_2 <- function(state, ot, col, num = "best") {
+        ## Assumes ot has already been loaded and converted to numeric and sorted.
+        ## col should already be the fully qualified column name from the original data set.
+        
+        ## Extract records for the supplied state
+        state_only <- ot[ot$State == state,]
+        
+        ## Remove NA values
+        valid_only <- state_only[!is.na(state_only[,col]),]
+        
+        if (num == "best") {
+                num <- 1
+        } else if (num == "worst") {
+                num <- nrow(valid_only)
+        }
+        
+        if (num > nrow(valid_only)) {
+                return(NA)
+        } else {
+                return(toString(valid_only[num, "Hospital.Name"]))
+        }
+}
+
+
 rankall <- function(outcome, num = "best") {
+
         ## Read outcome data
-
-        ## Check that state and outcome are valid
-
-        ## For each state, find the hospital of the given rank
-        ## Return a data frame with the hospital names and the
-        ## (abbreviated) state name
+        ot<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        ## Lame string parsing...
+        if (outcome == "heart attack") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+        } else if (outcome == "heart failure") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"                
+        } else if (outcome == "pneumonia") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        } else {
+                stop("invalid outcome")
+        }
+        
+        ## Convert numbers from strings to actual numbers.
+        ot[,col] <- as.numeric(ot[,col])
+        
+        ## Extract all valid states?unique
+        state <- unique(ot$State)
+        state <- state[order(state)]
+        
+        ## Sort the table
+        ot <- ot[order(ot[,col], ot$Hospital.Name),]
+ 
+        hospital = sapply(state, rankhospital_2, ot, col, num)
+        
+        data.frame(cbind(hospital, state))
 }
--- rprog/assignment_3/rankhospital.R
+++ rprog/assignment_3/rankhospital.R
@@ -1,8 +1,45 @@
 rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
-
+        ot<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
         ## Check that state and outcome are valid
+        if (!sum(ot$State==state)) {
+                stop("invalid state")        
+        }
+        
+        ## Lame string parsing...
+        if (outcome == "heart attack") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+        } else if (outcome == "heart failure") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"                
+        } else if (outcome == "pneumonia") {
+                col = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        } else {
+                stop("invalid outcome")
+        }
+        
+        ## Convert numbers from strings to actual numbers.
+        ot[,col] <- as.numeric(ot[,col])
+        
+        ## Sort the table
+        ot_sorted <- ot[order(ot[,col], ot$Hospital.Name),]
+        
+        ## Extract records for the supplied state
+        state_only <- ot_sorted[ot_sorted$State == state,]
+        
+        ## Remove NA values
+        valid_only <- state_only[!is.na(state_only[,col]),]
+        
+        if (num == "best") {
+                num <- 1
+        } else if (num == "worst") {
+                num <- nrow(valid_only)
+        }
 
-        ## Return hospital name in that state with the given rank
-        ## 30-day death rate
+        message(num)
+        if (num > nrow(valid_only)) {
+                return(NA)
+        } else {
+                return(toString(valid_only[num, "Hospital.Name"]))
+        }
 }
